e130a8e99317af6420a55074e5968c7a02d18da2
Bruno Soares <brunohenrique.soares@outlook.com>
2022-07-11 22:40:15 -0300

    Criação do botão de notificação #Commit
    Primeiro componente em React criado, botão de notificação com imagem;

---
+++ frontend/src/componentes/buttonNotification/index.tsx
@@ -0,0 +1,11 @@
+import NotificationImage from "../../assets/images/icon-notification.svg"
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={NotificationImage} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
