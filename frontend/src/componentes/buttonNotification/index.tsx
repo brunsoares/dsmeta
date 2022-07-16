b508473330a643a4d8ac0e48bc9a313487628437
Bruno Soares <brunohenrique.soares@outlook.com>
2022-07-16 14:53:01 -0300

    Integração front e back #Commit
    Alterações:
    - Enviando notificação pelo botão do front;

--- frontend/src/componentes/buttonNotification/index.tsx
+++ frontend/src/componentes/buttonNotification/index.tsx
@@ -1,9 +1,22 @@
 import NotificationImage from "../../assets/images/icon-notification.svg";
+import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
 import './style.css';
 
-function NotificationButton() {
+type Props={
+    saleId: number;
+}
+
+function handleClick(id: number){
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+         .then(response => {
+            console.log("Notificação enviada!")
+         });
+}
+
+function NotificationButton({saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={NotificationImage} alt="Notificar" />
         </div>
     )
